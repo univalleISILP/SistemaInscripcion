b46356cac179373e6bfcaa65e9f674fccd7cd349
CristhianSilvaMelgar <crissilva.cs77@gmail.com>
2019-06-01 11:49:08 -0400

    f

---
+++ SistemaInscripcion/Docente.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInscripcion
+{
+    class Docente
+    {
+        public int CI { get; set; }
+        public String Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public char Genero { get; set; }
+        public string Especialidad { get; set; }
+
+        public int Telefono { get; set; }
+
+        public string Direccion { get; set; }
+
+        public Docente() { }
+
+        public Docente(int ci, string nombre, string apellido, char genero, string especialidad, int telefono, string direccion)
+        {
+            CI = ci;
+            Nombre = nombre;
+            Apellido = apellido;
+            Genero = genero;
+            Especialidad = especialidad;
+            Telefono = telefono;
+            Direccion = direccion;
+        }
+
+    }
+}
---
+++ SistemaInscripcion/Materia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInscripcion
+{
+    class Materia
+    {
+        public int ClaveM { get; set; }
+        public String Nombre { get; set; }
+
+        public Materia() { }
+        public Materia(int claveM,string nombre)
+        {
+            ClaveM = claveM;
+            Nombre = nombre;
+        }
+    }
+}

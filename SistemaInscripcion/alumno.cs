2c939aa1dee24755ecab05181a2654d07c11c92c
USUARIO <jorgeluisf231@gmail.com>
2019-06-01 11:23:10 -0400

    clase usuario

---
+++ SistemaInscripcion/alumno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInscripcion
+{
+    public class alumno
+    {
+        public int CI_Alumno { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public char Genero { get; set; }
+        public int Edad { get; set; }
+        public string Direccion { get; set; }
+        public string Grado { get; set; }
+        public string AñoCurso { get; set; }
+
+
+        public alumno(){}
+
+        public alumno(int ciAlum,string nom, string apell,char geb, int ed, string dir, string grad, string aCur)
+        {
+            CI_Alumno = ciAlum;
+            Nombre = nom;
+            Apellido = apell;
+            Genero = geb;
+            Edad = ed;
+            Direccion = dir;
+            Grado = grad;
+            AñoCurso = aCur;
+        }
+    }
+}
---
+++ SistemaInscripcion/usuario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInscripcion
+{
+    public class usuario
+    {
+        public string CodUsuario { get; set; }
+        public string Tipo { get; set; }
+        public string Contraseña { get; set; }
+
+
+        public usuario() { }
+
+        public usuario(string codUser, string tip, string pswd) {
+
+            CodUsuario = codUser;
+            Tipo = tip;
+            Contraseña = pswd;
+        }
+    } 
+}

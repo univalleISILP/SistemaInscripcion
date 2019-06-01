bb45fe4b9b7725da04850da7eaf0abf5d27a1182
USUARIO <jorgeluisf231@gmail.com>
2019-06-01 10:48:08 -0400

    parte dos

--- SistemaInscripcion/PlataformaUser.cs
+++ SistemaInscripcion/PlataformaUser.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SistemaInscripcion
 {
-    public partial class Form1 : Form
+    public partial class PlataformaUser : Form
     {
-        public Form1()
+        public PlataformaUser()
         {
             InitializeComponent();
         }

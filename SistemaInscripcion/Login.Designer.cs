ff6e025912e5964422c83d16f8cdff6aa63d4fc0
USUARIO <jorgeluisf231@gmail.com>
2019-06-01 11:45:24 -0400

    b

--- SistemaInscripcion/Login.Designer.cs
+++ SistemaInscripcion/Login.Designer.cs
@@ -33,12 +33,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(249, 78);
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button1.Location = new System.Drawing.Point(254, 78);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(210, 158);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Login
             // 

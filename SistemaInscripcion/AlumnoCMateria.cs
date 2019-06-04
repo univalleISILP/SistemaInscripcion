585ae3d67526e28f60d26d37b1d9c360816b6a19
CristhianSilvaMelgar <crissilva.cs77@gmail.com>
2019-06-04 13:54:35 -0400

    consultas

---
+++ SistemaInscripcion/AlumnoCMateria.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInscripcion
+{
+    public class AlumnoCMateria
+    {
+        public string nombre { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Nota3 { get; set; }
+        public double Notaf { get; set; }
+        public string NomDocente { get; set; }
+
+        public AlumnoCMateria() { }
+        public AlumnoCMateria(string nom,double n1, double n2, double n3, double nf, string nd)
+        {
+            nombre = nom;
+            Nota1 = n1;
+            Nota2 = n2;
+            Nota3 = n3;
+            Notaf = nf;
+            NomDocente = nd;
+        }
+    }
+}
--- SistemaInscripcion/plataformaAlumnoABML.cs
+++ SistemaInscripcion/plataformaAlumnoABML.cs
@@ -37,27 +37,28 @@ namespace SistemaInscripcion
             return lista;
         }
 
-        public static List<ListaDatosAlumno> ListarDatosTodoAlu(int ci)
+        public static List<AlumnoCMateria> ListarDatosTodoAlu(int ci)
         {
-            List<ListaDatosAlumno> lista = new List<ListaDatosAlumno>();
+            List<AlumnoCMateria> lista = new List<AlumnoCMateria>();
 
             using (SqlConnection conexion = Conexion.ObtenerConexion1())
             {
 
                 SqlCommand comando = new SqlCommand(string.Format(@"
-                SELECT n.Nota1,n.Nota2,n.Nota3,n.Notaf,d.nombre as docente
+                SELECT m.nombre,n.Nota1,n.Nota2,n.Nota3,n.Notaf,d.nombre as docente
                 FROM Modulo n,Materias m,Docentes d,Alumnos a	
                 Where a.CI_Alumno = {0} and a.CI_Alumno=n.CI_Alumno and d.CI_Docente=n.CI_Docente and m.Clave_Materia=n.Clave_Materia", ci), conexion);
                 SqlDataReader leer = comando.ExecuteReader();
 
                 while (leer.Read())
                 {
-                    ListaDatosAlumno alumno = new ListaDatosAlumno();
-                    alumno.Nota1 = leer.GetDouble(0);
-                    alumno.Nota2 = leer.GetDouble(1);
-                    alumno.Nota3 = leer.GetDouble(2);
-                    alumno.Notaf = leer.GetDouble(3);
-                    alumno.NomDocente = leer.GetString(4);
+                    AlumnoCMateria alumno = new AlumnoCMateria();
+                    alumno.nombre = leer.GetString(0);
+                    alumno.Nota1 = leer.GetDouble(1);
+                    alumno.Nota2 = leer.GetDouble(2);
+                    alumno.Nota3 = leer.GetDouble(3);
+                    alumno.Notaf = leer.GetDouble(4);
+                    alumno.NomDocente = leer.GetString(5);
                     lista.Add(alumno);
                 }
                 conexion.Close();
